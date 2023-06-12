953e75959329982cde031a6b17cac546d5a4fb0a
Antje Witte <antjewitte90@gmail.com>
2023-06-12 12:25:15 +0200

    fertig

--- src/components/list.js
+++ src/components/list.js
@@ -1,10 +1,32 @@
-export default function List({ activities, id }) {
+export default function List({ activities, isActive, onDeleteActivity }) {
+  console.log(onDeleteActivity);
   return (
-    <section>
-      <h2>Activities:</h2>
-      <ul>
-        <li key={id}>{activities}</li>
-      </ul>
-    </section>
+    <>
+      <section>
+        <h2>
+          {isActive === true
+            ? "🌤️ Outdoor activities:"
+            : "🌧️ Indoor activities:"}
+        </h2>
+        <ul>
+          {activities
+            .filter((activity) => activity.isForGoodWeather === isActive)
+            .map((activity) => {
+              return (
+                <li key={activity.id}>
+                  {activity.name}
+                  <button
+                    type="button"
+                    title="delete movie"
+                    onClick={() => onDeleteActivity(activity.id)}
+                  >
+                    ✕
+                  </button>
+                </li>
+              );
+            })}
+        </ul>
+      </section>
+    </>
   );
 }
